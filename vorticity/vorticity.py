62ccf35498fe50a8c9b57a14dc510be073c243b3
Daria-Levitchi <dlevitchi@tudelft.nl>
2026-03-19 09:44:15 +0100

    check

--- vorticity/vorticity.py
+++ vorticity/vorticity.py
@@ -1,13 +1,40 @@
 import pandas as pd
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+import numpy as np
+=======
+=======
+>>>>>>> 039f6103187495b2d8f5f283efcca4c1702204be
+=======
+>>>>>>> 039f6103187495b2d8f5f283efcca4c1702204be
+=======
+>>>>>>> a61984d8df727e3054eaedafdbff4cfa773e02ca
+>>>>>>> Stashed changes
 import numpy as np
 
 
 #reading the x,y,z coordinates
 dfu = pd.read_csv('Vorticity/U_gradient_results.csv')
+<<<<<<< Updated upstream
 x = dfu['x_coords'].values
 y = dfu['y_coords'].values
 z = dfu['z_coords'].values
 
+=======
+<<<<<<< HEAD
+x = dfu['x'].values
+y = dfu['y'].values
+z = dfu['z'].values
+=======
+x = dfu['x_coords'].values
+y = dfu['y_coords'].values
+z = dfu['z_coords'].values
+>>>>>>> a61984d8df727e3054eaedafdbff4cfa773e02ca
+>>>>>>> Stashed changes
 
 #readig the U_gradient
 du_dx = dfu['du_dx'].values
@@ -45,4 +72,21 @@ df_out = pd.DataFrame({
     'omega_z': omega_z
 })
 
+<<<<<<< Updated upstream
 df_out.to_csv('Vorticity/vorticity_xyz.csv', index=False)
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+df_out.to_csv('Vorticity/vorticity_xyz.csv', index=False)
+>>>>>>> 039f6103187495b2d8f5f283efcca4c1702204be
+=======
+df_out.to_csv('Vorticity/vorticity_xyz.csv', index=False)
+>>>>>>> 039f6103187495b2d8f5f283efcca4c1702204be
+=======
+df_out.to_csv('Vorticity/vorticity_xyz.csv', index=False)
+>>>>>>> 039f6103187495b2d8f5f283efcca4c1702204be
+=======
+df_out.to_csv('Vorticity/vorticity_xyz.csv', index=False)
+>>>>>>> a61984d8df727e3054eaedafdbff4cfa773e02ca
+>>>>>>> Stashed changes
