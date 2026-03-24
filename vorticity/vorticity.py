dcc5bf065e51df39617c30ce5612a32df7d83458
Junior-DD <ogikg178@gmail.com>
2026-03-24 14:42:02 +0100

    Added vorticity columns to gradient_results.csv

--- vorticity/vorticity.py
+++ vorticity/vorticity.py
@@ -2,36 +2,32 @@ import pandas as pd
 import numpy as np
 
 # reading the x,y,z coordinates
-df = pd.read_csv('Vorticity/gradient_results.csv')
+df = pd.read_csv('Vorticity/gradient_results_backup.csv')
 
 # reading the U_gradient
-du_dx = df['du_dx'].values
-du_dy = df['du_dy'].values
-du_dz = df['du_dz'].values
+du_dx = df.iloc[:, 3]
+du_dy = df.iloc[:, 4]
+du_dz = df.iloc[:, 5]
 
 # reading the V_gradient
-dv_dx = df['dv_dx'].values
-dv_dy = df['dv_dy'].values
-dv_dz = df['dv_dz'].values
+dv_dx = df.iloc[:, 6]
+dv_dy = df.iloc[:, 7]
+dv_dz = df.iloc[:, 8]
 
 # reading the W_gradient
-dw_dx = df['dw_dx'].values
-dw_dy = df['dw_dy'].values
-dw_dz = df['dw_dz'].values
+dw_dx = df.iloc[:, 9]
+dw_dy = df.iloc[:, 10]
+dw_dz = df.iloc[:, 11]
 
 # calculating the vorticity components
 omega_x = dw_dy - dv_dz
 omega_y = du_dz - dw_dx
 omega_z = dv_dx - du_dy
 
-# creating a dataframe to store the results
-df_out = pd.DataFrame({
-    'x': x,
-    'y': y,
-    'z': z,
-    'omega_x': omega_x,
-    'omega_y': omega_y,
-    'omega_z': omega_z
-})
+# adding the collumns to the dataframe
+df['omega_x2'] = dw_dy - dv_dz
+df['omega_y2'] = du_dz - dw_dx
+df['omega_z2'] = dv_dx - du_dy
 
-df_out.to_csv('Vorticity/vorticity_xyz.csv', index=False)
+#saving the results to a new csv file
+df.to_csv('Vorticity/gradient_results_backup.csv', index=False)
